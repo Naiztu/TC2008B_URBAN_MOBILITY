34031c038f69865f3c80c1f43f88767a411c4a43
LeilaelRico <A01707023@tec.mx>
2022-11-25 19:22:24 -0600

    Json information retrieved / Typos corrected.

--- Mobility_Simulation/Assets/Scripts/UrbanMobility.cs
+++ Mobility_Simulation/Assets/Scripts/UrbanMobility.cs
@@ -6,9 +6,17 @@ using TMPro;
 public class UrbanMobility : MonoBehaviour
 {
     public TextMeshProUGUI agentText;
+
+    //public int arr = {1, 2, 3, 6};
     public void NewAgent(){
         Agent a = APIHelper.GetNewAgent();
         agentText.text = a.step_count.ToString();
+
+        for(int i = 0; i < a.positions.Count; i++)
+        {
+            Debug.Log("X: " + a.positions[i].position.x);
+           
+        }
 
         Debug.Log("Height = " + a.height + "\nMax_Carros = " + a.max_num_cars + "\nTiempo = " + a.time);
 
